c59ccff5ada54439ed323ac1152af6830f4c0409
Michelle <Michelle.Tinajero@outlook.com>
2020-08-13 17:35:43 -0500

    added files

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code away!
+const express = require("express")
+const logger = require("./middleware/logger")
+const userRouter = require("./users/userRouter")
+
+const server = express()
+const port = 5000
+
+server.use(express.json)
+server.use(logger("long"))
+server.use(userRouter)
+
+server.listen(port, () => {
+    console.log (`server running at http://localhost:${port}`)
+})
